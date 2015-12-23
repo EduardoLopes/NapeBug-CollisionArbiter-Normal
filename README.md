91acf16007ef81ecfff58e59992b6d46308fa06c
Eduardo Lopes <EduardoLopesDS@gmail.com>
2015-12-23 16:27:22 -0300

    Update 2015-12-23T19:27:18.464Z

--- README.md
+++
@@ -1,9 +0,0 @@
-# Nape's Collision Arbiter Normals Swapped Bug Test
-
-
-
-## How to compile
-
-Setup [openfl](http://www.openfl.org/download/)
-
-run `openfl test neko`
--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-const ghPages = require('gulp-gh-pages');
-
-gulp.task('deploy-flash', function() {
-  return gulp.src(['./Export/flash/bin/**/*', './flash_test/**/*'])
-    .pipe(ghPages());
-});
